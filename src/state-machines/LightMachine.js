ad8e222eb18ef5d3881016d9b3aa0ceaebd57e2d
denisseai <msdenisseanaya@gmail.com>
2020-05-14 12:49:21 -0700

    wave 1 implement a state machine blueprint xstate

--- src/state-machines/LightMachine.js
+++ src/state-machines/LightMachine.js
@@ -1,9 +1,25 @@
 import { createMachine, interpret } from 'xstate';
 
 const lightMachine = createMachine({
-  // Your state machine blueprint here
-
+  id: 'changeLight',
+  initial: 'red',
+  states: {
+    red: { on: { changeLight: 'green'} },
+    green: { on: { changeLight: 'yellow'} },
+    yellow: { on: { changeLIght: 'red'} }
+  }
 });
+
+// This ... works with xstate
+// const lightMachine = Machine({
+//   id: 'changeLight',
+//   initial: 'red',
+//   states: {
+//     red: { on: { changeLight: 'green'} },
+//     green: { on: { changeLight: 'yellow'} },
+//     yellow: { on: { changeLIght: 'red'} }
+//   }
+// });
 
 
 const service = interpret(lightMachine);
